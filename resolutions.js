13514f93ebba87fd6b3104495481bae70c59fc14
Sachin Singh <singhsachin87@yahoo.com>
2016-01-21 14:11:34 +0530

    remove insecure package

--- resolutions.js
+++ resolutions.js
@@ -31,10 +31,10 @@ if (Meteor.isClient) {
 
   Template.resolution.events({
     'click .toggle-checked': function(){
-      Resolutions.update(this._id, {$set: {checked: !this.checked}});
+      Meteor.call("updateResolution", this._id, !this.checked)
     },
     'click .delete': function(){
-      Resolutions.remove(this._id);
+      Meteor.call('deleteResolution', this._id);
     }
   })
 
@@ -50,10 +50,16 @@ if (Meteor.isServer) {
 }
 
 Meteor.methods({
-  'addResolutions': function(title){    
-      Resolutions.insert({
-        title: title,
-        createdAt: new Date()
-      });
+  addResolutions: function(title){    
+    Resolutions.insert({
+      title: title,
+      createdAt: new Date()
+    });
+  },
+  updateResolution: function(id, checked){
+    Resolutions.update(id, {$set: {checked: checked}});
+  },
+  deleteResolution: function(id){
+    Resolutions.remove(id);
   }
 })
